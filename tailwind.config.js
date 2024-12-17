f6380800bb3dc2597a1d05ce6f998b2c8188c12e
ManhNT <themanh127@gmail.com>
2024-12-17 23:24:06 +0700

    responsive header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,22 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        "light-coffee":'#C89F94'
-      }
+      fontFamily: {
+        Karla: ["Karla", "sans-serif"],
+      },
+      colors: {
+        "light-coffee": "#C89F94",
+      },
+      keyframes: {
+        slideDown: {
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        slideDown: "slideDown .4s ease-in-out",
+      },
     },
   },
   plugins: [],
-}
-
+};
